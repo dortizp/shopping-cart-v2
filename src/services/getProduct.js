1f8681f0216d97788812bd2ca76f436621886b84
David Ortiz <david.ortiz.p@gmail.com>
2023-01-05 08:50:33 -0500

    refactor of getProduct

--- src/services/getProduct.js
+++ src/services/getProduct.js
@@ -1,16 +1,16 @@
 const getProduct  = () => {
-    setInterval(
-        () => {
-              const product = {
-                title: "milk",
-                description: "box of milk 1L",
-                price: 10
-            }
-            console.log("product", product)
-            return product
-        }
-        ,3000
-    )
+  const product = {
+    title: "milk",
+    description: "box of milk 1L",
+    price: 10
+    }
+    // setTimeout(
+        // () => {
+        console.log("product", product)
+        return product
+        // }
+        // ,3000
+        // )
 }
 
 export {getProduct}
